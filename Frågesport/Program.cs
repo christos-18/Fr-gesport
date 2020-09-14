e80ea0bb68b9be4a2219a4730fd5ddf602af997f
christos-18 <potamitischristos@gmail.com>
2020-09-14 09:50:19 +0200

    1

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -6,7 +6,33 @@ namespace Frågesport
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string fråga1 = "";
+
+            while (fråga1 != "a" && fråga1 != "b" && fråga1 != "c")
+
+
+            {
+                Console.WriteLine("vad heter jag");
+                fråga1 = Console.ReadLine();
+            }
+            if (fråga1 = "b")
+            {
+                Console.WriteLine("Det var rätt! Du har just nu "x" poäng");
+            }
+
+            Console.ReadLine();
+
+             else if (fråga1 != "b")
+
+            {
+                Console.WriteLine("Det var fel! Du har just nu poäng);
+            }
+
+            Console.ReadLine();
+
+
+
+
         }
     }
 }
